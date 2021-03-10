4f39f1639fdc2ab60e7b4756bad561bafd1f776b
Jonathan Robijn <jprobijn@gmail.com>
2021-03-10 23:25:36 +0100

    Updated script to send alerts over Telegram

--- main.py
+++ main.py
@@ -1,58 +1,26 @@
+import dotenv
+import os
 import configparser
-import getpass
-import pykeepass
 
 import time
 import requests
 import lxml.etree as etree
 import hashlib
-import logging
 
-import smtplib
-import email
+import telebot 
 
 
 def load_config(path='config.txt'):
     config = configparser.ConfigParser()
     config.read(path)
 
-    keepass = config['keepass']
-    smtp = config['smtp']
     monitor = config['monitor']
 
-    return keepass, smtp, monitor
+    return monitor['url'], monitor['xpath'], int(monitor['interval'])
 
 
-def load_keepass_database(path):
-    master_pw = getpass.getpass(prompt='What is the Keepass database master password? ')
-    return pykeepass.PyKeePass(path, password=master_pw)
-
-
-def get_smtp_credentials_from_db(keepass_db, entry_path):
-    credentials = keepass_db.find_entries(path=entry_path.split('/'), first=True)
-
-    if not credentials:
-        raise ValueError(f'Could not find credentials in loaded Keepass database')
-
-    return {
-        'username': credentials.username,
-        'password': credentials.password
-    }
-
-
-def send_email(smtp_server, smtp_user, subject, msg):
-    server = smtplib.SMTP(smtp_server['url'], smtp_server['port'])
-    server.starttls()
-    server.login(smtp_user['username'], smtp_user['password'])
-
-    email_msg = email.message.EmailMessage()
-    email_msg.set_content(msg)
-    email_msg['Subject'] = subject
-    email_msg['From'] = smtp_user['username']
-    email_msg['To'] = smtp_user['username']
-
-    server.send_message(email_msg)
-    server.quit()
+def send_telegram(bot, msg):
+    bot.send_message(1623987917, msg)
 
 
 def monitor_webpage_section(url, xpath, interval, alert_func):
@@ -67,7 +35,6 @@ def monitor_webpage_section(url, xpath, interval, alert_func):
         page_title = doc_tree.xpath('//title/text()')[0]
 
         new_md5 = hashlib.md5(etree.tostring(page_section)).hexdigest()
-
         if current_md5 and new_md5 != current_md5:
             alert_func(
                 f'{page_title} --- Page updated',
@@ -81,11 +48,12 @@ def monitor_webpage_section(url, xpath, interval, alert_func):
 
 
 if __name__ == '__main__':
-    keepass, smtp_server, monitor = load_config()
-    keepass_db = load_keepass_database(keepass['db_path'])
-    smtp_user = get_smtp_credentials_from_db(keepass_db, keepass['entry_path'])
+    url, xpath, interval = load_config()
 
-    alert_func = lambda title, message: send_email(smtp_server, smtp_user, title, message)
+    dotenv.load_dotenv()
+    bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
 
-    monitor_webpage_section(monitor["url"], monitor["xpath"], int(monitor["interval"]), alert_func)
+    # Telegram doesn't support message titles
+    alert_func = lambda title, message: send_telegram(bot, message)
 
+    monitor_webpage_section(url, xpath, interval, alert_func)
